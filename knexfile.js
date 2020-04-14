86c8b2eeff68a8f7024e7fc1718e528d4259405f
Derek <legato_5@yahoo.com>
2020-04-13 18:55:10 -0700

    creating basic backend with unit test.

---
+++ knexfile.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL_DEV,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+  testing: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL_TEST,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+};
+
