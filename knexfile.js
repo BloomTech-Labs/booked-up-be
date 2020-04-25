48a7ce91fd3c3b80f8b5675a2adadb82d0fd8a48
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-04-24 23:07:04 -0700

    connected posgres db locally for development, knex igrate working

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   development: {
     client: 'pg',
-    connection: process.env.DATABASE_URL_DEV,
+    connection: process.env.LOCALDB,
     useNullAsDefault: true,
     migrations: {
       directory: './data/migrations',
