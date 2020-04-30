9dbe0c076e337795c3b84f028065503983603c91
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-04-29 23:41:03 -0700

    register/ login validation. added express-validation. all validations and error handling for sign up and logging in complete

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
 const express = require('express')
-const configureMiddleware = require('./configure-middleware.js')
-const authRouter = require('../auth/auth-router');
+const configureMiddleware = require('./configure-middleware.js');
+const registerRouter = require('../auth/register-router');
+const loginRouter = require('../auth/login-router');
 const usersRouter = require('../users/user-router');
 const server = express();
 
 configureMiddleware(server)
 
-server.use('/api/auth', authRouter);
+server.use('/api/auth/register', registerRouter);
+server.use('/api/auth/login', loginRouter);
 server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
