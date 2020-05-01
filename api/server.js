a4f213690e6cfd2ff4027c56be94e5e924cccf17
Daniel Cruz <d.cruz.489@hotmail.com>
2020-04-30 22:15:47 -0600

    Added author content model and router

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const configureMiddleware = require('./configure-middleware.js');
 const registerRouter = require('../auth/register-router');
 const loginRouter = require('../auth/login-router');
 const usersRouter = require('../users/user-router');
+const contentRouter = require('../author-content/content-router');
 const server = express();
 
 configureMiddleware(server)
@@ -10,6 +11,7 @@ configureMiddleware(server)
 server.use('/api/auth/register', registerRouter);
 server.use('/api/auth/login', loginRouter);
 server.use('/api/users', usersRouter);
+server.use('/api/author-content', contentRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({api: "Booked Up server live."})
---
+++ author-content/content-model.js
@@ -0,0 +1,30 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+    get,
+    add,
+    update,
+    deleteContent
+}
+
+function get(){
+    return db('author_content');
+}
+
+function add(newContent){
+    return db('author_content').insert(newContent).returning("*");
+}
+
+function update(content, id){
+    return db('author_content').where({ id }).update(content).returning("*");
+}
+
+function deleteContent(id){
+    return db('author_content').where({ id }).delete();
+}
+
+
+
+
+
+
