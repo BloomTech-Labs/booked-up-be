0a4fc7ca56a4ff5455162b40aed861d3c16f32ae
Daniel Cruz <d.cruz.489@hotmail.com>
2020-05-04 21:12:14 -0600

    adding content library code

--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const registerRouter = require('../auth/register-router');
 const loginRouter = require('../auth/login-router');
 const usersRouter = require('../users/user-router');
 const contentRouter = require('../author-content/content-router');
+const libraryRouter = require('../content-library/library-router');
 const server = express();
 
 configureMiddleware(server)
@@ -12,6 +13,7 @@ server.use('/api/auth/register', registerRouter);
 server.use('/api/auth/login', loginRouter);
 server.use('/api/users', usersRouter);
 server.use('/api/author-content', contentRouter);
+server.use('/api/content-library', libraryRouter);
 
 server.get('/', (req, res) => {
     res.status(200).json({api: "Booked Up server live."})
---
+++ content-library/library-model.js
@@ -0,0 +1,24 @@
+const db = require('../data/dbConfig.js');
+
+module.exports = {
+    get,
+    add,
+    update,
+    deleteFavorite
+}
+
+function get(){
+    return db('content_library');
+}
+
+function add(newFavorite){
+    return db('content_library').insert(newFavorite).returning("*");
+}
+
+function update(favorite, id){
+    return db('content_library').where({ id }).update(favorite).returning("*");
+}
+
+function deleteFavorite(id){
+    return db('content_library').where({ id }).delete();
+}
