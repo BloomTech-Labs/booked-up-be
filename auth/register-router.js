9835b150ed29447d01b5be92bb833f8bcce283d9
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-05-12 22:07:45 -0700

    removed returning user on register

--- auth/register-router.js
+++ auth/register-router.js
@@ -46,7 +46,7 @@ router.post('/', [
             Users.add(user)
                 .then(u => {
                     // sendConfirmationEmail(u)
-                    res.status(201).json({message: "email sent", user: u})
+                    res.status(201).json({message: "email sent"})
                 })
                 .catch(err => {
                     res.status(500).json(err.message)
