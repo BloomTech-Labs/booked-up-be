ee9cc187056a6707e78bdc28cbaa5696e8e4881a
Daniel  Cruz <danielcruz489@gmail.com>
2020-05-27 21:20:04 -0600

    updated migration for comments, messages and author_content

--- data/migrations/20200527204416_creat-comments_messages.js
+++ data/migrations/20200527204416_creat-comments_messages.js
@@ -1,5 +1,16 @@
-exports.up = function (knex) {
+exports.up = async (knex) => {
     return knex.schema
+
+        .table("author_content", (tbl) => {
+            tbl.string("description", 255)
+            tbl.string("img_url");
+        })
+
+        .table("content_library", (tbl) => {
+            tbl.dropColumn("id")
+            tbl.primary(["user_id", "author_content_id"]);
+        })
+
         .createTable("comments", tbl => {
             tbl.increments();
             tbl.string("comment", 300).notNullable();
@@ -38,7 +49,7 @@ exports.up = function (knex) {
         .createTable("messages", tbl => {
             tbl.increments();
             tbl.string("subject", 255);
-            tbl.string("body", 255);
+            tbl.string("body", 1020).notNullable();
             tbl.timestamp("created_at").defaultTo(knex.fn.now());
             tbl.boolean("success_verification").defaultTo(false);
             tbl
@@ -58,7 +69,6 @@ exports.up = function (knex) {
         })
 
         .createTable("message-inbox", tbl => {
-            tbl.increments();
             tbl
                 .integer("user_id")
                 .unsigned()
@@ -73,15 +83,16 @@ exports.up = function (knex) {
                 .references("messages.id")
                 .onUpdate("CASCADE")
                 .onDelete("CASCADE");
-
+            tbl.primary(["user_id", "message_id"]);
         })
-
 }
 
 
-exports.down = function (knex) {
+exports.down = async (knex) => {
     return knex.schema
         .dropTableIfExists("message-inbox")
         .dropTableIfExists("messages")
         .dropTableIfExists("comments")
+        .dropTableIfExists("content_library")
+        .dropTableIfExists("author_content")
 };
