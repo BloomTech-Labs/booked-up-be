c99cc0e165303401b757d36a5e476966e9473ef6
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-06-14 13:28:48 -0700

    removed cloudinary confif from content router

--- author-content/content-router.js
+++ author-content/content-router.js
@@ -17,12 +17,6 @@ const {
   getContentByIdComments,
 } = require("./content-controller");
 
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API,
-  api_secret: process.env.CLOUDINARY_SECRET,
-});
-
 // Get all content
 
 router.get("/", restricted, getContent);
