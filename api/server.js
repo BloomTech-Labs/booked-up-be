73476a89322748af63c51c8bd55cb78cc98152bb
cauld19 <50849710+cauld19@users.noreply.github.com>
2020-06-14 13:21:47 -0700

    moved routes from server into api-router file

--- api/server.js
+++ api/server.js
@@ -1,32 +1,13 @@
 const express = require("express");
+const bookedUpRouter = require("./api-router.js");
 const configureMiddleware = require("./configure-middleware.js");
-const registerRouter = require("../auth/register-router");
-const loginRouter = require("../auth/login-router");
-const usersRouter = require("../users/user-router");
-const contentRouter = require("../author-content/content-router");
-const libraryRouter = require("../content-library/library-router");
-const adminRegisterRouter = require("../auth/admin-register-router");
-const adminLoginRouter = require("../auth/admin-login-router");
-const adminRouter = require("../admins/admin-router");
-const userResetPassword = require("../users/user-reset-password");
-const messageRouter = require("../messaging/message-router");
-const commentsRouter = require("../comments/comments-router");
 
 const server = express();
+
 server.set("view engine", "ejs");
 configureMiddleware(server);
 
-server.use("/api/auth/register", registerRouter);
-server.use("/api/auth/login", loginRouter);
-server.use("/api/users", usersRouter);
-server.use("/api/author-content", contentRouter);
-server.use("/api/content-library", libraryRouter);
-server.use("/api/auth/admin/register", adminRegisterRouter);
-server.use("/api/auth/admin/login", adminLoginRouter);
-server.use("/api/admin", adminRouter);
-server.use("/api/users/password", userResetPassword);
-server.use("/api/message/", messageRouter);
-server.use("/api/comments", commentsRouter);
+server.use("/api", bookedUpRouter);
 
 server.get("/", (req, res) => {
   res.status(200).json({
